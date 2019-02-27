29d53cc2f652ffec194e30e1560e9d35318539f4
Rony <ronymmoura@gmail.com>
2019-02-26 22:53:55 -0300

    Criando projeto

---
+++ DMN_SIM_NAO.cs
@@ -0,0 +1,8 @@
+﻿namespace Intech.Lib.Dominios
+{
+    public static class DMN_SIM_NAO
+    {
+        public const string SIM = "SIM";
+        public const string NAO = "NAO";
+    }
+}
---
+++ DMN_SN.cs
@@ -0,0 +1,8 @@
+﻿namespace Intech.Lib.Dominios
+{
+    public static class DMN_SN
+    {
+        public const string SIM = "S";
+        public const string NAO = "N";
+    }
+}
