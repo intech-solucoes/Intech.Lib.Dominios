eb186eb8e8a4e95817b5b8836f7530f547d43668
Rony Moura <ronymmoura@gmail.com>
2021-06-11 13:39:51 -0300

    Criando DMN_TIPO_REGISTRO

---
+++ EfdReinf/DMN_TIPO_REGISTRO.cs
@@ -0,0 +1,11 @@
+﻿namespace Intech.Lib.Dominios.EfdReinf
+{
+    public static class DMN_TIPO_REGISTRO
+    {
+        public const string R1000 = "R1000";
+        public const string R1070 = "R1070";
+        public const string R2010 = "R2010";
+        public const string R2098 = "R2098";
+        public const string R2099 = "R2099";
+    }
+}
